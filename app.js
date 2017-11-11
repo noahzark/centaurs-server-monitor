dc5cb6eaeafda80b28f8584f43065e64b840c50b
Yuancheng Zhang <old.far.hot@gmail.com>
2017-11-11 13:43:45 -0600

    changes listen port and add two APIs to receive plugin info

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ var os = require('os'),
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 10021
+
+app.set('port', port);
 
 app.use('/', express.static(__dirname + '/www'))
 
 app.use(bodyParser.json())
 
-app.get('/api/gm/server', function(req, res) {
+app.get('/api/gm/server', function (req, res) {
+
     var memUsage = util.inspect(process.memoryUsage()) + ''
-    
+
     var memStrs = memUsage.split(',')
 
     var info = {}
 
     var str = memStrs[0];
-    info.srv_alc = (parseInt(str.substring(str.indexOf(":")+1)) / 1024).toFixed()
-    info.srv_free = (info.srv_alc - parseInt(memStrs[2].substring(memStrs[2].indexOf(':')+1)) / 1024).toFixed()
-    
-    info.sys_free = (os.freemem()/1024).toFixed()
-    info.sys_sum = (os.totalmem()/1024).toFixed()
+    info.srv_alc = (parseInt(str.substring(str.indexOf(":") + 1)) / 1024).toFixed()
+    info.srv_free = (info.srv_alc - parseInt(memStrs[2].substring(memStrs[2].indexOf(':') + 1)) / 1024).toFixed()
+
+    info.sys_free = (os.freemem() / 1024).toFixed()
+    info.sys_sum = (os.totalmem() / 1024).toFixed()
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -31,4 +35,44 @@ app.get('/api/gm/server', function(req, res) {
     res.send(JSON.stringify(info))
 })
 
-server.listen(80, '0.0.0.0')
+app.post('/api/gm/server-info/', function (req, res, err) {
+    try {
+        if (!req.body) return res.sendStatus(400)
+
+        var info = req.body;
+        console.log(info);
+        res_obj = {};
+        res_obj.retcode = 0;
+        res_obj.msg = "success"
+        res.send(JSON.stringify(res_obj));
+    } catch (err) {
+        console.error(err);
+        res_obj = {};
+        res_obj.retcode = 1;
+        res_obj.msg = "request error"
+        res.send(JSON.stringify(res_obj));
+    }
+});
+
+app.post('/api/gm/test-info/', function (req, res, err) {
+    try {
+        if (!req.body) return res.sendStatus(400)
+        
+        var info = req.body;
+        console.log(info);
+        res_obj = {};
+        res_obj.retcode = 0;
+        res_obj.msg = "success"
+        res.send(JSON.stringify(res_obj));
+    } catch (err) {
+        console.error(err);
+        res_obj = {};
+        res_obj.retcode = 1;
+        res_obj.msg = "request error"
+        res.send(JSON.stringify(res_obj));
+    }
+});
+
+server.listen(port, function () {
+    console.log('Server started at ' + port);
+})
