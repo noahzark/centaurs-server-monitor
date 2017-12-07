a4d026f4067003eb0689f8673609e86d10da951b
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-07 17:50:29 -0600

    add getErrLog(), getTestLog(), getUsageLog()

--- services/log.js
+++ services/log.js
@@ -46,6 +46,20 @@ module.exports.addTestLog = function (log, callback) {
 }
 
 /**
+ * get app test log from db
+ * @param {string} app_name 
+ * @param {number} limit 
+ * @param {function} callback 
+ */
+module.exports.getTestLog = (app_name, limit, callback) => {
+	if (app_name) {
+		var query = { 'app_name': app_name };
+		var sort = { 'createdAt': -1 };
+		TestLog.find(query).sort(sort).limit(limit).exec(callback);
+	}
+}
+
+/**
  * add test result log into db
  * @param {Object} log - test log from route
  * @param {Function} callback 
@@ -56,6 +70,20 @@ module.exports.addErrLog = function (log, callback) {
 }
 
 /**
+ * get app err log from db
+ * @param {string} app_name 
+ * @param {number} limit 
+ * @param {function} callback 
+ */
+module.exports.getErrLog = (app_name, limit, callback) => {
+	if (app_name) {
+		var query = { 'app_name': app_name };
+		var sort = { 'createdAt': -1 };
+		ErrLog.find(query).sort(sort).limit(limit).exec(callback);
+	}
+}
+
+/**
  * add api time usage log into db
  * @param {Object} log - test log from route
  * @param {Function} callback 
@@ -63,4 +91,18 @@ module.exports.addErrLog = function (log, callback) {
 module.exports.addUsageLog = function (log, callback) {
 	var log_ins = new UsageLog(log);
 	log_ins.save(callback);
+}
+
+/**
+ * get app time usage log from db
+ * @param {string} app_name 
+ * @param {number} limit 
+ * @param {function} callback 
+ */
+module.exports.getUsageLog = (app_name, limit, callback) => {
+	if (app_name) {
+		var query = { 'app_name': app_name };
+		var sort = { 'createdAt': -1 };
+		UsageLog.find(query).sort(sort).limit(limit).exec(callback);
+	}
 }
