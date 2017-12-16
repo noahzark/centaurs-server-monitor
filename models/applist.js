23d23ac4a6005563f5759fcab4f7ff3d06d067bf
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-15 19:49:15 -0600

    add applist in db

---
+++ models/applist.js
@@ -0,0 +1,22 @@
+/**
+ * @file centaurs-server-monitor: Uncaught Exception Log Schema
+ * @copyright Centaurs Technologies Co. 2017
+ * @author Zhang, Yuancheng
+ * @license Unlicense
+ * @module models/applist
+ */
+
+var mongoose = require('mongoose');
+
+var ApplistSchema = mongoose.Schema({
+	app_name: {
+        type: String,
+    },
+    status: {
+		type: String,
+	}
+}, {
+    timestamps: true
+});
+
+var Applist = module.exports = mongoose.model('Applist', ApplistSchema);
